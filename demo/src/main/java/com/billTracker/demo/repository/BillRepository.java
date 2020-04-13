5724c30f2db1b47172e5eb6de2479aeb220e4001
Xucheng Zhao <xucheng@Xuchengs-MBP.attlocal.net>
2020-04-12 20:30:53 -0700

    Rewrite git ignore

--- demo/src/main/java/com/billTracker/demo/repository/BillRepository.java
+++
@@ -1,8 +0,0 @@
-package com.billTracker.demo.repository;
-
-
-import org.springframework.data.repository.CrudRepository;
-import com.billTracker.demo.model.Bill;
-public interface BillRepository extends CrudRepository<Bill, Long>{
-
-}
